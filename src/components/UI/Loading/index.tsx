91f3482f81ee73d85c8f6b10eadaeb4a5de21061
zakiego <belummikir@gmail.com>
2023-01-08 13:34:37 +0800

    feat: add title on loading

--- src/components/UI/Loading/index.tsx
+++ src/components/UI/Loading/index.tsx
@@ -1,18 +1,21 @@
 import { Box } from "@chakra-ui/react";
 
+import { PageWrapper } from "../../Layout";
 import { LoadingStyle } from "./style";
 
 export const Loading = () => {
   return (
-    <Box
-      alignItems="center"
-      justifyContent="center"
-      display="flex"
-      minH="100vh"
-    >
-      <LoadingStyle>
-        <Box className="dot-flashing" />
-      </LoadingStyle>
-    </Box>
+    <PageWrapper title="Loading | Portal Desa Bambangin">
+      <Box
+        alignItems="center"
+        justifyContent="center"
+        display="flex"
+        minH="100vh"
+      >
+        <LoadingStyle>
+          <Box className="dot-flashing" />
+        </LoadingStyle>
+      </Box>
+    </PageWrapper>
   );
 };
