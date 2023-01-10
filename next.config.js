81d54dd410f97b351dc816e972b63e1849612003
zakiego <belummikir@gmail.com>
2023-01-11 00:38:11 +0800

    feat: remove security header

--- next.config.js
+++ next.config.js
@@ -46,10 +46,10 @@ const securityHeaders = [
     value: ContentSecurityPolicy.replace(/\n/g, ""),
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: "Referrer-Policy",
-    value: "origin-when-cross-origin",
-  },
+  // {
+  //   key: "Referrer-Policy",
+  //   value: "origin-when-cross-origin",
+  // },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
     key: "X-Frame-Options",
