b8ad330a4c5718d633cc4da81fe1befb8f9227ce
zakiego <belummikir@gmail.com>
2023-01-11 00:30:44 +0800

    feat: add document umami

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="bdce2435-160e-466a-b1ed-aaa184281eca"
+          src="https://monitor.zakidev.my.id/umami.js"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
